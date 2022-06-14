998572fcf8d44314fd305eedc56a05c3984efa57
moro <ys7.xxxa@gmail.com>
2022-06-14 22:45:51 +0900

    [finish]basic_4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name;
+		
+		name = "Rena";
+		System.out.println(name);
+		
+		name = "morozumi";
+		System.out.println(name);
+		
+		final int number = 8;
+		System.out.println(number);
+	}
+
+}
---
+++ JavaWcp/src/Variable01.java
@@ -0,0 +1,22 @@
+
+public class Variable01 {
+	public static void main(String[] args) {
+	//変数の宣言
+		//基本構文「型 変数名;」	
+		int num1;
+		String str1;
+	//初期化・・宣言した変数に初期値を代入すること
+		//基本構文「型 変数名 = 代入するデータ;」	
+		num1 = 10;
+		str1 = "Hello World!";
+		System.out.println(num1);
+		System.out.println(str1);
+		
+		num1 = 20;
+		System.out.println(num1);
+		
+		int num2 = 30;
+		System.out.println(num2);
+	}
+
+}
