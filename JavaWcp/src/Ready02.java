bee32a17496bee70fed08b995e1689b349b1cddd
moro <ys7.xxxa@gmail.com>
2022-06-14 22:15:27 +0900

    [finish]basic_3

--- JavaWcp/src/Ready02.java
+++ JavaWcp/src/Ready02.java
@@ -1,6 +1,11 @@
 
+//クラスブロックの始まり
 public class Ready02 {
+	//メソッドブロックの始まり
 	public static void main(String[] args) {
+		//メソッドの処理を記述	
 		System.out.println("Hello World!");
+	//メソッドブロックの終わり	
 	}
+//クラスブロックの終わり
 }
